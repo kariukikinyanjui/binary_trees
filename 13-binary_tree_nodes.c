8217d61aa0045ae8a39cdddc75d463da606c8157
kariukikinyanjui <michaelkariuki7@gmail.com>
2023-11-01 08:06:30 +0000

    [Fixed]: Betty

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -15,7 +15,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 
 	if (tree->left != NULL || tree->right != NULL)
 	{
-		return (1 +binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+		return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
 	}
 
 	return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
