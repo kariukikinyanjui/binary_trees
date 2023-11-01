696396d839af6bc890a40e6a6fdc0a7621cba368
kariukikinyanjui <michaelkariuki7@gmail.com>
2023-11-01 14:37:30 +0000

    [Feature]: Updated header file

--- binary_trees.h
+++ binary_trees.h
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
+#include <string.h>
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -20,6 +21,7 @@ struct binary_tree_s
 };
 typedef struct binary_tree_s binary_tree_t;
 
+
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
@@ -33,6 +35,7 @@ int binary_tree_is_root(const binary_tree_t *node);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_print(const binary_tree_t *);
 size_t binary_tree_height(const binary_tree_t *tree);
 size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
