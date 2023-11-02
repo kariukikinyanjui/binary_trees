b1f637b13046b41c7e61d4a119cd499450b779ab
kariukikinyanjui <michaelkariuki7@gmail.com>
2023-11-02 16:12:41 +0000

    [Feature]: Function that checks if a binary tree is perfect

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_perfect - a function that checks if a binary tree is perfect
+ * @tree: a pointer to the root node of the tree to check
+ * Return: 0 if tree is NULL
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	int left_depth, right_depth;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	left_depth = binary_tree_height(tree->left);
+	right_depth = binary_tree_height(tree->right);
+
+	if (left_depth == right_depth)
+	{
+		if (tree->left == NULL && tree->right == NULL)
+		{
+			return (1);
+		}
+		return (binary_tree_is_perfect(tree->left) &&
+				binary_tree_is_perfect(tree->right));
+	}
+	return (0);
+}
